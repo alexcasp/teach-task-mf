e9bccc9600f8342cf8a2e829b4edbab280f86b11
alxkrstv <48849566+alexcasp@users.noreply.github.com>
2025-01-13 17:42:32 +0300

    Delete Is a number prime directory

--- Is a number prime/Is a number prime.py
+++
@@ -1,8 +0,0 @@
-#Is a number prime?
-def is_prime(num):
-    if num < 2:
-        return False
-    for i in range(2, int(num**0.5) + 1):  # Проверяем только до sqrt(n)
-        if num % i == 0:
-            return False
-    return True
